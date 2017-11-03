b3a0ca430fdad514fac1e57831511a6f3238dfbc
jian <1342042894@qq.com>
2017-11-03 10:55:03 +0800

    add hupai

--- demo/main.cpp
+++ demo/main.cpp
@@ -235,7 +235,23 @@ void vInit()
 
 struct Sub {
     long long a;
+    long long arr[14];
+    int arrLen;
+
     long long b;
+    long long brr[14];
+    int brrLen;
+
+    Sub()
+    {
+        a = 0;
+        memset(arr, 0, sizeof(arr));
+        arrLen = 0;
+
+        b = 0;
+        memset(brr, 0, sizeof(brr));
+        brrLen = 0;
+    }
 };
 
 int iGetNumBits(long long iNum)
@@ -260,65 +276,172 @@ int iGetNumLeftBit(long long iNum)
     return iNum / (pow(10, iBitsNum - 1));
 }
 
+void vReverseNumArray(long long arr[], int iArrLen)
+{
+    int iStart = 0;
+    int iEnd = iArrLen - 1;
+    while (iStart < iEnd)
+    {
+        long long tmp = arr[iStart];
+        arr[iStart] = arr[iEnd];
+        arr[iEnd] = tmp;
+
+        ++iStart;
+        --iEnd;
+    }
+}
+
 Sub SplitNum(long long iNum, int i)
 {
     int iSplitLine = pow(10, i);
     Sub sub;
     sub.a = iNum / iSplitLine;
     sub.b = iNum % iSplitLine;
+
+    long long numArr[14] = { 0 };
+    int iNumArrLen = 0;
+    long long sourceNum = iNum;
+    while (sourceNum > 0)
+    {
+        numArr[iNumArrLen++] = sourceNum % 10;
+        sourceNum /= 10;
+    }
+
+    int k = 0;
+    while(k < i)
+    {
+        sub.brr[sub.brrLen++] = numArr[k++];
+    }
+    //vReverseNumArray(sub.brr, sub.brrLen);
+
+    while (k < iNumArrLen)
+    {
+        sub.arr[sub.arrLen++] = numArr[k++];
+    }
+
     return sub;
 }
 
 void CalcMainSub(long long iNum, Sub MainSub, vector<Sub> & subVec)
 {
-    long long iRightBitsNum = iGetNumBits(MainSub.b);
+    int iLeftNum = MainSub.arrLen >= 2 ? 2 : MainSub.arrLen;
+    int iRightNum = MainSub.brrLen >= 2 ? 2 : MainSub.brrLen;
 
-    long long iNewLeft = MainSub.a / 100;
-    long long iNewRight = 0;
-    if (MainSub.b > 100)
+    if (iLeftNum == 1)
     {
-        iNewRight = MainSub.b % long long(pow(10, iRightBitsNum - 2));
-    }
+        int l1 = MainSub.arr[0];
+        Sub sub;
+        sub.a = 1;
 
-    long long iLeft1 = MainSub.a % 10;
-    long long iLeft2 = MainSub.a / 10 % 10;
+        sub.b = MainSub.b + pow(10, MainSub.brrLen);
 
-    long long iRight1 = MainSub.b / (pow(10, iRightBitsNum - 1));
-    long long iRight2 = 0;
-    if (iRightBitsNum - 2 >= 0)
-    {
-        iRight2 = (long long)(MainSub.b / (pow(10, iRightBitsNum - 2))) % 10;
-    }
-
-    for (int i = 0; i <= iLeft1; i++)
-    {
-        for (int j = 0; j <= iLeft2; j++)
+        if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
         {
-            Sub sub;
-            sub.a = iNewLeft * 100 + i * 10 + j;
-            sub.b = (iLeft1 - i)*pow(10, iRightBitsNum + 1) + (iLeft2 - j) * pow(10, iRightBitsNum) + MainSub.b;
-
-            if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+            subVec.push_back(sub);
+        }
+    }
+    else if (iLeftNum == 2)
+    {
+        int l1 = MainSub.arr[0];
+        int l2 = MainSub.arr[1];
+        for (int i = 0; i <= l1; i++)
+        {
+            for (int j = 0; j <= l2; j++)
             {
-                subVec.push_back(sub);
+                Sub sub;
+                sub.a = MainSub.a / 100 + i * 10 + j;
+                sub.b = MainSub.b + (l1 - i) * pow(10, MainSub.brrLen) + (l2 - j) * pow(10, MainSub.brrLen + 1);
+
+                if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+                {
+                    subVec.push_back(sub);
+                }
             }
         }
     }
 
-    for (int i = 0; i <= iRight1; i++)
+    if (iRightNum == 1)
     {
-        for (int j = 0; j <= iRight2; j++)
-        {
-            Sub sub;
-            sub.a = MainSub.a * 100 + (iRight1 - i) * 10 + (iRight2 - j);
-            sub.b = i*pow(10, iRightBitsNum - 1) +  j * pow(10, iRightBitsNum - 2) + iNewRight;
+        int r1 = MainSub.brr[0];
+        Sub sub;
+        sub.a = MainSub.a * 10 + 1;
 
-            if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+        sub.b = 1;
+
+        if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+        {
+            subVec.push_back(sub);
+        }
+    }
+    else if (iRightNum == 2)
+    {
+        int r1 = MainSub.brr[MainSub.brrLen - 1];
+        int r2 = MainSub.brr[MainSub.brrLen - 2];
+        for (int i = 0; i <= r1; i++)
+        {
+            for (int j = 0; j <= r2; j++)
             {
-                subVec.push_back(sub);
+                Sub sub;
+                sub.a = MainSub.a * 100 + (r1 - i) * 10 + (r2 - j);
+
+                sub.b = MainSub.b % ((long long)pow(10, MainSub.brrLen - 2)) + i * pow(10, MainSub.brrLen - 1) + j * pow(10, MainSub.brrLen - 2);
+
+                if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+                {
+                    subVec.push_back(sub);
+                }
             }
         }
     }
+
+    //long long iRightBitsNum = iGetNumBits(MainSub.b);
+
+    //long long iNewLeft = MainSub.a / 100;
+    //long long iNewRight = 0;
+    //if (MainSub.b > 100)
+    //{
+    //    iNewRight = MainSub.b % long long(pow(10, iRightBitsNum - 2));
+    //}
+
+    //long long iLeft1 = MainSub.a % 10;
+    //long long iLeft2 = MainSub.a / 10 % 10;
+
+    //long long iRight1 = MainSub.b / (pow(10, iRightBitsNum - 1));
+    //long long iRight2 = 0;
+    //if (iRightBitsNum - 2 >= 0)
+    //{
+    //    iRight2 = (long long)(MainSub.b / (pow(10, iRightBitsNum - 2))) % 10;
+    //}
+
+    //for (int i = 0; i <= iLeft1; i++)
+    //{
+    //    for (int j = 0; j <= iLeft2; j++)
+    //    {
+    //        Sub sub;
+    //        sub.a = iNewLeft * 100 + j * 10 + i;
+    //        sub.b = (iLeft2 - j)*pow(10, iRightBitsNum + 1) + (iLeft1 - i) * pow(10, iRightBitsNum) + MainSub.b;
+
+    //        if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+    //        {
+    //            subVec.push_back(sub);
+    //        }
+    //    }
+    //}
+
+    //for (int i = 0; i <= iRight1; i++)
+    //{
+    //    for (int j = 0; j <= iRight2; j++)
+    //    {
+    //        Sub sub;
+    //        sub.a = MainSub.a * 100 + (iRight1 - i) * 10 + (iRight2 - j);
+    //        sub.b = i*pow(10, iRightBitsNum - 1) +  j * pow(10, iRightBitsNum - 2) + iNewRight;
+
+    //        if (sub.a > 0 && sub.a < iNum && sub.b > 0 && sub.b < iNum)
+    //        {
+    //            subVec.push_back(sub);
+    //        }
+    //    }
+    //}
 }
 
 void GetNumSub(long long iNum, vector<Sub> & subVec)
@@ -333,12 +456,64 @@ void GetNumSub(long long iNum, vector<Sub> & subVec)
     }
 }
 
+bool bSplitWithTwoBlank(long long iNum, vector<long long> & setNum)
+{
+    long long sourceNum = iNum;
+    long long iBase = 10;
+    long long iCurNum = sourceNum % 10;
+    sourceNum /= 10;
+    bool bPreIsZero = iCurNum == 0 ? true : false;
+    while (sourceNum > 0)
+    {
+        long long iCurBit = sourceNum % 10;
+        sourceNum /= 10;
+
+        if (iCurBit == 0 && bPreIsZero)
+        {
+            if (iCurNum > 0 && iCurNum < iNum)
+            {
+                setNum.push_back(iCurNum);
+            }
+            iCurNum = 0;
+            iBase = 1;
+            bPreIsZero = (iCurBit == 0) ? true : false;
+        }
+        else
+        {
+            iCurNum = iCurNum + iCurBit * iBase;
+            iBase *= 10;
+            bPreIsZero = (iCurBit == 0) ? true : false;
+        }
+    }
+
+    if (iCurNum > 0 && iCurNum < iNum)
+    {
+        setNum.push_back(iCurNum);
+    }
+
+    return setNum.size() > 0;
+}
+
 long long GetMinLaiZi(long long iNum)
 {
     map<long long, long long>::iterator iterMap = handCardsMap.find(iNum);
     if (iterMap != handCardsMap.end())
     {
         return iterMap->second;
+    }
+
+    vector<long long> setNum;
+    bool bRet = bSplitWithTwoBlank(iNum, setNum);
+    if (bRet)
+    {
+        int iMin = 0;
+        vector<long long>::iterator iter = setNum.begin();
+        while (iter != setNum.end())
+        {
+            iMin += GetMinLaiZi(*iter);
+            ++iter;
+        }
+        return iMin;
     }
 
     vector<Sub> subVec;
@@ -365,8 +540,24 @@ long long GetMinLaiZi(long long iNum)
 
 void vTest2()
 {
-    long long lRet = GetMinLaiZi(22122);
+    long long lRet = GetMinLaiZi(10121);
     printf("lRet = %lld\n", lRet);
+
+
+    vector< set<long long> >::iterator iterVec = vec.begin();
+    while (iterVec != vec.end())
+    {
+        set<long long> & curset = *iterVec;
+        set<long long>::iterator iterSet = curset.begin();
+        while (iterSet != curset.end())
+        {
+            printf("m(%lld) = %lld\n", *iterSet, GetMinLaiZi(*iterSet));
+
+            ++iterSet;
+        }
+        printf("\n\n");
+        ++iterVec;
+    }
 }
 
 
